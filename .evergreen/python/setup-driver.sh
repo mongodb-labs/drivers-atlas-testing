41bad7e1d4a991645f4143a92fa201273e0a17fa
Prashant Mital <prashantmital@gmail.com>
2020-03-06 20:15:42 -0800

    remove spurious code

--- .evergreen/python/setup-driver.sh
+++ .evergreen/python/setup-driver.sh
@@ -5,4 +5,3 @@ $PYTHON_BINARY -m virtualenv pymongotestvenv
 . pymongotestvenv/bin/activate
 pip install -e .[srv]
 
-$ASTROLABE --log-level debug spec-tests run temp-tests/ -e .evergreen/python/workload-executor.sh  --group-name testproject --cluster-name-salt somesalt
