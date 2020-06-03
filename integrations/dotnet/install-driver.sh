eb6288f71aefd5495d699250b0271fd7ae3e9fd0
vincentkam <vincent.kam@10gen.com>
2020-06-03 14:15:25 -0400

    CSHARP-2515: Test Planned Maintenance Scenarios in Drivers

---
+++ integrations/dotnet/install-driver.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -o xtrace   # Write all commands first to stderr
+set -o errexit  # Exit the script with error if any of the commands fail
+
+# Environment variables used as input:
+#   FRAMEWORK                       Set to specify .NET framework to test against. Values: "net452", "netcoreapp1.1",
+#                                   "netcoreapp2.1"
+
+dotnet --version
+dotnet --list-sdks
+
+# /p required to get around https://github.com/dotnet/sdk/issues/12159
+dotnet build mongo-csharp-driver /p:Platform="Any CPU" # platform needs a space when building
+dotnet publish mongo-csharp-driver/tests/AstrolabeWorkloadExecutor \
+    --no-build --no-restore \
+    --framework "${FRAMEWORK}" \
+    /p:Platform="AnyCpu" # platform does not need a space when publishing
