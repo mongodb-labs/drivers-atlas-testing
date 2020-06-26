d49668af50f7e06d6faec37d68d75ac6d79b8f33
Eric Adum <eric.adum@gmail.com>
2020-06-26 11:49:11 -0400

    node integration (#75)
    NODE-1867

---
+++ integrations/node/install-driver.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+
+# set -o xtrace   # Write all commands first to stderr
+set -o errexit  # Exit the script with error if any of the commands fail
+
+PWD=$(pwd)
+PROJECT_DIRECTORY="$PWD/node-mongodb-native"
+
+# make sure there are no node/npm in path
+PATH=$(echo "$PATH" | sed -e 's/:\/opt\/node\/bin$//')
+
+NODE_LTS_NAME=${NODE_LTS_NAME:-carbon}
+NODE_ARTIFACTS_PATH="${PROJECT_DIRECTORY}/node-artifacts"
+NPM_CACHE_DIR="${NODE_ARTIFACTS_PATH}/npm"
+NPM_TMP_DIR="${NODE_ARTIFACTS_PATH}/tmp"
+
+# this needs to be explicitly exported for the nvm install below
+export NVM_DIR="${NODE_ARTIFACTS_PATH}/nvm"
+export XDG_CONFIG_HOME=${NODE_ARTIFACTS_PATH}
+
+# create node artifacts path if needed
+mkdir -p ${NODE_ARTIFACTS_PATH}
+ls ${NODE_ARTIFACTS_PATH}
+mkdir -p ${NPM_CACHE_DIR}
+mkdir -p "${NPM_TMP_DIR}"
+
+# install nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.35.3/install.sh | bash
+[ -s "${NVM_DIR}/nvm.sh" ] && \. "${NVM_DIR}/nvm.sh"
+
+# install node
+nvm install --no-progress --lts=${NODE_LTS_NAME}
+
+# setup npm cache in a local directory
+cat <<EOT > .npmrc
+devdir=${NPM_CACHE_DIR}/.node-gyp
+init-module=${NPM_CACHE_DIR}/.npm-init.js
+cache=${NPM_CACHE_DIR}
+tmp=${NPM_TMP_DIR}
+registry=https://registry.npmjs.org
+EOT
+
+# install workload executor dependencies
+npm install yargs lodash.omit
+
+# install node dependencies
+cd ${PROJECT_DIRECTORY}
+npm install
+
---
+++ integrations/node/workload-executor.js
@@ -0,0 +1,111 @@
+#!/usr/bin/env node
+
+const MongoClient = require('../../node-mongodb-native').MongoClient;
+const fs = require('fs');
+const assert = require('assert');
+const omit = require('lodash.omit');
+
+const NOOP_DELAY = 1000;
+
+let interrupted = false;
+process.on('SIGINT', () => (interrupted = true));
+
+const operations = new Map();
+operations.set(
+  'find',
+  (collection, args) => collection.find(args.filter || {}, omit(args, ['filter'])).toArray()
+);
+operations.set(
+  'insertOne',
+  (collection, args) => collection.insertOne(args.document /*, { forceServerObjectId: true }*/)
+);
+operations.set(
+  'updateOne',
+  (collection, args) => collection.updateOne(args.filter, args.update)
+);
+
+async function main(uri, spec) {
+  const runOperations = makeRunOperations(spec);
+
+  const client = new MongoClient(uri);
+  try {
+    await client.connect();
+
+    const results = { numSuccesses: 0, numFailures: 0, numErrors: 0 };
+
+    while (!interrupted) {
+      try {
+        await runOperations(client, results);
+      } catch (err) {
+        ++results.numErrors;
+      }
+    }
+
+    fs.writeFileSync('results.json', JSON.stringify(results));
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.close();
+  }
+
+  process.exit(0);
+}
+
+function makeRunOperations(spec) {
+  const operations = spec.operations;
+  return function(client, results) {
+    if (!Array.isArray(operations) || operations.length === 0) {
+      return new Promise(resolve => setTimeout(resolve, NOOP_DELAY));
+    }
+
+    const database = client.db(spec.database);
+    const collection = database.collection(spec.collection);
+    return operations.reduce(
+      (chain, op) => chain.then(() => runOperation.call({ database, collection }, op))
+        .then(result => {
+          try {
+            if (op.result != null) {
+              assert.deepStrictEqual(result, op.result);
+            }
+            ++results.numSuccesses;
+          } catch (failure) {
+            ++results.numFailures;
+          }
+        })
+        .catch(() => (++results.numErrors)),
+      Promise.resolve()
+    );
+  };
+}
+
+function runOperation(op) {
+  const object = this[op.object];
+  if (!object) {
+    throw new Error(`Unsupported object: ${op.object}`);
+  }
+  if (!operations.has(op.name)) {
+    throw new Error(`Unsupported operation: ${op.name}`);
+  }
+  const operation = operations.get(op.name);
+  return operation(object, op.arguments);
+}
+
+
+const argv = require('yargs').command(
+  '$0 <connectionString> <workloadSpecification>',
+  '',
+  yargs => {
+    yargs
+      .positional('connectionString', {
+        describe: 'a connection string for the driver to connect to',
+        type: 'string'
+      })
+      .positional('workloadSpecification', {
+        describe: 'a JSON blob of operations to run during workload execution',
+        type: 'string'
+      });
+  }
+).argv;
+
+const spec = JSON.parse(argv.workloadSpecification);
+main(argv.connectionString, spec).catch(console.err);
