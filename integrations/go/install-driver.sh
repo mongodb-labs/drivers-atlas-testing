8c590ead99766c21b531b9d8e1a9abf05d7cb936
Divjot Arora <divjot.arora@10gen.com>
2020-07-15 17:28:29 -0400

    divjot changes

--- integrations/go/install-driver.sh
+++ integrations/go/install-driver.sh
@@ -2,4 +2,12 @@
 
 set -ex
 
-PATH=$GOROOT/bin:$PATH go build -o integrations/$DRIVER_DIRNAME/executor integrations/$DRIVER_DIRNAME/workload-executor.go
+echo "INSTALLING DRIVER"
+pwd
+cd integrations/$DRIVER_DIRNAME
+pwd
+
+export PATH=$GOROOT/bin:$PATH 
+go version
+go get go.mongodb.org/mongo-driver@master
+go build -o executor workload-executor.go
