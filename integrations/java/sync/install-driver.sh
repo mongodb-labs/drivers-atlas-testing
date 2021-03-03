95fc531810ff09571a57b4dc9fea74a06fd9973c
Jeff Yemin <jeff.yemin@mongodb.com>
2021-03-03 17:01:11 -0500

    Add workload executor for the synchronous Java driver

---
+++ integrations/java/sync/install-driver.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -o errexit
+
+cd mongo-java-driver || exit
+./gradlew --info driver-workload-executor:shadowJar
+cd ..
+cp mongo-java-driver/driver-workload-executor/build/libs/driver-workload-executor-*.jar driver-workload-executor.jar
+
