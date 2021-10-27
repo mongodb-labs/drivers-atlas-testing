58080fea0fd139a0f90079089ec4d517ce08f82c
DmitryLukyanov <dmitry.lukyanov@mongodb.com>
2021-10-27 23:32:20 +0300

    CSHARP-3909: Code review.

--- integrations/dotnet/install-driver.sh
+++ integrations/dotnet/install-driver.sh
@@ -5,12 +5,9 @@ set -o errexit  # Exit the script with error if any of the commands fail
 # Environment variables used as input:
 #   FRAMEWORK                       Set to specify .NET framework to test against. Values: "netcoreapp2.1"
 
-# temporary workaround until https://jira.mongodb.org/browse/BUILD-14126 is resolved
+curl -sSL https://dot.net/v1/dotnet-install.ps1 -o integrations/dotnet/dotnet-install.ps1
 # the below .ps1 script install .net50 into ${LOCALAPPDATA}/Microsoft/dotnet which we should use to build and publish the project
 powershell.exe '.\integrations\dotnet\dotnet-install.ps1 -Channel 5.0 '
-
-${LOCALAPPDATA}/Microsoft/dotnet/dotnet --version
-${LOCALAPPDATA}/Microsoft/dotnet/dotnet --list-sdks
 
 # /p required to get around https://github.com/dotnet/sdk/issues/12159
 ${LOCALAPPDATA}/Microsoft/dotnet/dotnet build mongo-csharp-driver /p:Platform="Any CPU" # platform needs a space when building
