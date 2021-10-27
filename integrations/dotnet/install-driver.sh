4c32c75068463527715e62bdce63e944ba4f74d2
Dmitry Lukyanov <dmitry.lukyanov@mongodb.com>
2021-10-28 00:29:49 +0300

    CSHARP-3909: Astrolabe is misconfigured. (#129)
    CSHARP-3909: Astrolabe is misconfigured.

--- integrations/dotnet/install-driver.sh
+++ integrations/dotnet/install-driver.sh
@@ -5,12 +5,13 @@ set -o errexit  # Exit the script with error if any of the commands fail
 # Environment variables used as input:
 #   FRAMEWORK                       Set to specify .NET framework to test against. Values: "netcoreapp2.1"
 
-dotnet --version
-dotnet --list-sdks
+curl -sSL https://dot.net/v1/dotnet-install.ps1 -o integrations/dotnet/dotnet-install.ps1
+# the below .ps1 script install .net50 into ${LOCALAPPDATA}/Microsoft/dotnet which we should use to build and publish the project
+powershell.exe '.\integrations\dotnet\dotnet-install.ps1 -Channel 5.0 '
 
 # /p required to get around https://github.com/dotnet/sdk/issues/12159
-dotnet build mongo-csharp-driver /p:Platform="Any CPU" # platform needs a space when building
-dotnet publish mongo-csharp-driver/tests/AstrolabeWorkloadExecutor \
+${LOCALAPPDATA}/Microsoft/dotnet/dotnet build mongo-csharp-driver /p:Platform="Any CPU" # platform needs a space when building
+${LOCALAPPDATA}/Microsoft/dotnet/dotnet publish mongo-csharp-driver/tests/AstrolabeWorkloadExecutor \
     --no-build --no-restore \
     --framework "${FRAMEWORK}" \
     /p:Platform="AnyCpu" # platform does not need a space when publishing
