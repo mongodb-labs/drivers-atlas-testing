7dd88abfd5b51681ee3367569a68557f780a0b48
julius <juliusgeo@gmail.com>
2021-11-09 14:26:35 -0800

    add filtering to get rid of type before writing to file, add in events.json logic

--- integrations/python/pymongo/workload-executor.py
+++ integrations/python/pymongo/workload-executor.py
@@ -7,6 +7,13 @@ import os
 from test.unified_format import UnifiedSpecTestMixinV1
 
 
+def filter_failures_errors(entity_map, target):
+    e, f = [], []
+    for i in entity_map[target]:
+        (e, f)[i["type"] == AssertionError].append(i)
+    entity_map._entities["failures"], entity_map._entities["errors"] = f, e
+
+
 def workload_runner(mongodb_uri, test_workload):
     # Run operations
     runner = UnifiedSpecTestMixinV1()
@@ -16,20 +23,15 @@ def workload_runner(mongodb_uri, test_workload):
     runner.setUp()
     for i in test_workload["tests"][0]["operations"]:
         if i.get("name") == "loop":
-            i["arguments"]["numIterations"] = 5
+            i["arguments"]["numIterations"] = 10
     runner.run_scenario(test_workload["tests"][0], uri=mongodb_uri)
     entity_map = runner.entity_map
     if "failures" not in entity_map and "errors" in entity_map:
-        entity_map["failures"] = [i for i in entity_map["errors"] if i[
-            "type"] == AssertionError]
-        entity_map._entities["errors"] = [i for i in entity_map["errors"] if i[
-            "type"] != AssertionError]
+        filter_failures_errors(entity_map, "errors")
     if "errors" not in entity_map and "failures" in entity_map:
-        entity_map["failures"] = [i for i in entity_map["errors"] if i[
-            "type"] == AssertionError]
-        entity_map._entities["errors"] = [i for i in entity_map["errors"] if i[
-            "type"] != AssertionError]
-    for i in ["errors", "failures"]:
+        filter_failures_errors(entity_map, "failures")
+
+    for i in ["errors", "failures", "events"]:
         if i not in entity_map:
             entity_map[i] = []
     for i in ["successes", "iterations"]:
@@ -39,13 +41,16 @@ def workload_runner(mongodb_uri, test_workload):
     results = {"numErrors": len(entity_map["errors"]), "numFailures":
         len(entity_map["failures"]), "numSuccesses": entity_map["successes"],
                "numIterations": entity_map["iterations"]}
-
+    for i in range(len(entity_map["failures"])):
+        entity_map["failures"][i].pop("type")
+    for i in range(len(entity_map["errors"])):
+        entity_map["errors"][i].pop("type")
     events = {"events": entity_map["events"], "errors": entity_map[
         "errors"], "failures": entity_map["failures"]}
-    print("Workload statistics: {!r}".format(results))
-    print("Workload events: {!r}".format(events))
+    #print("Workload statistics: {!r}".format(results))
+    #print("Workload events: {!r}".format(events))
     sentinel = os.path.join(os.path.abspath(os.curdir), 'results.json')
-    print("Writing statistics to sentinel file {!r}".format(sentinel))
+    #print("Writing statistics to sentinel file {!r}".format(sentinel))
     with open('results.json', 'w') as fr:
         json.dump(results, fr)
     with open('events.json', 'w') as fr:
