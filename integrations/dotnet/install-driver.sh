b4899b9ccdc6230551d3f51beff8fd5b8604b9a7
Matt Dale <9760375+matthewdale@users.noreply.github.com>
2022-04-21 08:48:29 -0700

    Add retry to downloading the dotnet install script.

--- integrations/dotnet/install-driver.sh
+++ integrations/dotnet/install-driver.sh
@@ -5,7 +5,16 @@ set -o errexit  # Exit the script with error if any of the commands fail
 # Environment variables used as input:
 #   FRAMEWORK                       Set to specify .NET framework to test against. Values: "netcoreapp2.1"
 
-curl -sSL https://dot.net/v1/dotnet-install.ps1 -o integrations/dotnet/dotnet-install.ps1
+# Download the dotnet installation script, retrying up to 5 times on any errors.
+curl -sSL \
+    --max-time 20 \
+    --retry 5 \
+    --retry-delay 0 \
+    --retry-max-time 60 \
+    --retry-all-errors \
+    -o integrations/dotnet/dotnet-install.ps1 \
+    https://dot.net/v1/dotnet-install.ps1
+
 # the below .ps1 script install .net50 into ${LOCALAPPDATA}/Microsoft/dotnet which we should use to build and publish the project
 powershell.exe '.\integrations\dotnet\dotnet-install.ps1 -Channel 5.0 '
 
