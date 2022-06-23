373b675620b8331c732824e27478ed5b8d9b3def
Isabel Atkinson <isabel.atkinson@mongodb.com>
2022-06-23 15:21:03 -0400

    install rust in working directory

--- integrations/rust/install-driver.sh
+++ integrations/rust/install-driver.sh
@@ -3,11 +3,15 @@
 set -o errexit
 set -o xtrace
 
-rm -rf ~/.rustup
+export WORKING_DIRECTORY=$(pwd)
+
+export RUSTUP_HOME="$WORKING_DIRECTORY/.rustup"
+export CARGO_HOME="$WORKING_DIRECTORY/.cargo"
+
 curl https://sh.rustup.rs -sSf | sh -s -- -y --no-modify-path
 
-echo "export CARGO_NET_GIT_FETCH_WITH_CLI=true" >> ~/.cargo/env
+echo "export CARGO_NET_GIT_FETCH_WITH_CLI=true" >> $WORKING_DIRECTORY/.cargo/env
 
-source $HOME/.cargo/env
+source $WORKING_DIRECTORY/.cargo/env
 
 cargo build --manifest-path mongo-rust-driver/Cargo.toml --tests --release
