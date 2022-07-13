32a731331085ed361397e7a5dea2dad75f663a01
Matt Dale <9760375+matthewdale@users.noreply.github.com>
2022-07-12 22:51:51 -0700

    DRIVERS-2256 Start a Kind cluster on Evergreen.

--- kubernetes/kind/create.sh
+++ kubernetes/kind/create.sh
@@ -5,6 +5,8 @@ set -e
 # This script creates a Kind cluster, installs the MongoDB Community Kubernetes Operator, and starts
 # a 3-node MongoDB replicaset.
 
+SCRIPT_DIR="$(dirname "$0")"
+
 # Checks if the binary is available, either in $PATH or at the explicit path provided.
 function is_binary_available {
   builtin type -P "$1" &> /dev/null
@@ -26,11 +28,8 @@ is_binary_available $CMCTL || (echo "Failed to find cmctl at '$CMCTL'" && exit 1
 JQ=${JQ:-jq}
 is_binary_available $JQ || (echo "Failed to find jq at '$JQ'" && exit 1)
 
-# Change directory to the script directory.
-cd "$(dirname "$0")"
-
 # Create local Kind cluster with specific host-port mapping. Wait for up to 5 minutes.
-$KIND create cluster --config kind.yml --wait 5m
+$KIND create cluster --config $SCRIPT_DIR/kind.yml --wait 5m
 
 # Add helm repositories for the MongoDB Community Kubernetes Operator and the Cert Manager Operator.
 $HELM repo add mongodb https://mongodb.github.io/helm-charts
@@ -58,15 +57,15 @@ $CMCTL --namespace cert-manager check api --wait=2m
 
 # Create the necessary configmap and TLS secret for Cert Manager to generate a new TLS certificate
 # for our MongoDB servers and clients.
-$KUBECTL --namespace default create configmap ca-config-map --from-file=ca.crt=./rootCA.pem
-$KUBECTL --namespace default create secret tls ca-key-pair  --cert=./rootCA.pem  --key=./rootCA-key.pem
+$KUBECTL --namespace default create configmap ca-config-map --from-file=ca.crt=$SCRIPT_DIR/rootCA.pem
+$KUBECTL --namespace default create secret tls ca-key-pair  --cert=$SCRIPT_DIR/rootCA.pem  --key=$SCRIPT_DIR/rootCA-key.pem
 
 # Create the Cert Manager certificate issuer and certificate
-$KUBECTL --namespace default apply -f cert-manager-issuer.yml
-$KUBECTL --namespace default apply -f cert-manager-certificate.yml
+$KUBECTL --namespace default apply -f $SCRIPT_DIR/cert-manager-issuer.yml
+$KUBECTL --namespace default apply -f $SCRIPT_DIR/cert-manager-certificate.yml
 
 # Create the MongoDB 3-node replicaset.
-$KUBECTL --namespace default apply -f mongodb.yml
+$KUBECTL --namespace default apply -f $SCRIPT_DIR/mongodb.yml
 
 # Wait up to 10 minutes for the MongoDB replicaset to be in phase "Running". Sleep for 10 seconds
 # before trying to wait because sometimes the "status" key isn't part of the resource definition
@@ -75,9 +74,6 @@ sleep 10
 $KUBECTL --namespace default wait MongoDBCommunity/mongodb \
     --for=jsonpath='{.status.phase}'=Running \
     --timeout=10m
-
-# Change directory back to the original directory
-cd -
 
 # Download the TLS certificate and key from the TLS secret created by Cert Manager. Decode them with
 # jq and concatenate them together to match the format expected by MongoDB drivers.
