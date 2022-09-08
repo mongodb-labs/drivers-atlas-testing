c3b06f7f6483a31af1d3175fac1ebcf9a136a3fc
Matt Dale <9760375+matthewdale@users.noreply.github.com>
2022-09-08 09:49:49 -0700

    DRIVERS-2420 Use a keystore to provide TLS certs to the Java driver. (#146)

--- integrations/java/sync/install-driver.sh
+++ integrations/java/sync/install-driver.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
+set -o xtrace
 set -o errexit
-
-export JAVA_HOME="/opt/java/jdk17"
 
 cd mongo-java-driver || exit
 ./gradlew --info driver-workload-executor:shadowJar
 cd ..
 cp mongo-java-driver/driver-workload-executor/build/libs/driver-workload-executor-*.jar driver-workload-executor.jar
-
