b08d49f915041f2f0745e5b0867991e7cf6bee71
Preston Vasquez <24281431+prestonvasquez@users.noreply.github.com>
2023-12-11 15:16:41 -0700

    GODRIVER-3069 Update Go integration to use V1

--- integrations/go/install-driver.sh
+++ integrations/go/install-driver.sh
@@ -7,6 +7,6 @@ cd integrations/$DRIVER_DIRNAME
 
 export PATH=$GOROOT/bin:$PATH
 
-go get go.mongodb.org/mongo-driver@master
+go get go.mongodb.org/mongo-driver@v1
 go mod tidy
 go test -c workload_executor_test.go -o executor
