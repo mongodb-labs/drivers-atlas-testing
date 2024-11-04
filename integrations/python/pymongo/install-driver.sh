94c9978f5696911b4cebcda0cfe2fad4babb3f61
Matt Dale <9760375+matthewdale@users.noreply.github.com>
2024-11-04 12:53:22 -0800

    Fix missing workload executor expansions in test runner function.

--- integrations/python/pymongo/install-driver.sh
+++ integrations/python/pymongo/install-driver.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -o xtrace
 
+PYMONGO_VIRTUALENV_NAME=${PYMONGO_VIRTUALENV_NAME:-pymongotestvenv}
+
 "$PYTHON_BINARY" --version
 "$PYTHON_BINARY" -m virtualenv "$PYMONGO_VIRTUALENV_NAME"
 "$PYMONGO_VIRTUALENV_NAME/$PYTHON_BIN_DIR/pip" install -e mongo-python-driver
